8d1d44e19e3a6092c0ba698af953dfa017f2ecde
Keith Morrison <keithm@infused.org>
2011-03-01 22:06:31 -0800

    Remove old test directory

--- test/unit/base_test.rb
+++
@@ -1,9 +0,0 @@
-require File.dirname(__FILE__) + '/../test_helper'
-
-class Stater::BaseTest < Test::Unit::TestCase
-  
-  def test_sum_consecutive_integers
-    assert_equal 300, Stater::Base.sum_consecutive_integers(24)
-  end
-  
-end
