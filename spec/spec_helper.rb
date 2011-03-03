69789aab27aa05b8b2a018788dd7ae4b38c848bf
Keith Morrison <keithm@infused.org>
2011-03-02 19:51:47 -0800

    Cleanup tvalue_schedule matcher

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,14 +11,12 @@ module CustomMatchers
     control_schedule = []
     doc = Nokogiri(open(File.dirname(__FILE__) + "/fixtures/tvalue_schedules/#{xml_file}"))
 
-    lines = (doc/:tvalueamortizationschedule/:amortizationline)
-    lines.each do |line|
-      if line.search(:amortizationlinetype).inner_text == '9'
-        payment           = line.search(:payment1amount).inner_text.gsub(/(\d{2})\d{2}$/, '.\1')
-        interest_paid     = line.search(:interestpaid).inner_text.gsub(/(\d{2})\d{2}$/, '.\1')
-        principal_paid    = line.search(:principalpaid).inner_text.gsub(/(\d{2})\d{2}$/, '.\1')
-        principal_balance = line.search(:principalbalance).inner_text.gsub(/(\d{2})\d{2}$/, '.\1')
-        
+    (doc/:amortizationline).each do |line|
+      if (line/:amortizationlinetype).inner_text == '9'
+        payment           = (line/:payment1amount).inner_text.gsub(/(\d{2})\d{2}$/, '.\1')
+        interest_paid     = (line/:interestpaid).inner_text.gsub(/(\d{2})\d{2}$/, '.\1')
+        principal_paid    = (line/:principalpaid).inner_text.gsub(/(\d{2})\d{2}$/, '.\1')
+        principal_balance = (line/:principalbalance).inner_text.gsub(/(\d{2})\d{2}$/, '.\1')
         control_schedule << Struct::Payment.new(payment.to_d, principal_paid.to_d, interest_paid.to_d, principal_balance.to_d)
       end
     end
