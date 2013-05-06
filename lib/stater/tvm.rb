2f379fe01c8cfc9e1c4d7df25eddbc2d80e717b6
Keith Morrison <keithm@infused.org>
2013-05-05 23:05:31 -0700

    fix spelling error

--- lib/stater/tvm.rb
+++ lib/stater/tvm.rb
@@ -1,7 +1,7 @@
 module Stater
   class TVM
     include Math
-    
+
     class << self
       # Calculates the future value
       #   pv = present value
@@ -11,7 +11,7 @@ module Stater
       def fv(pv, i, n, m)
         pv * (1 + (i/m)) ** (n*m)
       end
-      
+
       # Calulates the present value
       #   fv = future value
       #   i = periodic interest rate
@@ -20,8 +20,8 @@ module Stater
       def pv(fv, i, n, m)
         fv / (1 + (i/m)) ** (n*m)
       end
-      
-      # Calculates the nomical interest rate
+
+      # Calculates the nominal interest rate
       #   pv = present value
       #   fv = future value
       #   y = years
@@ -29,7 +29,7 @@ module Stater
       def i(pv, fv, y, n)
         n * ((fv / pv) ** (1 / (y.to_f * n)) - 1)
       end
-    
+
       # Calculates the number of years
       #   pv = present value
       #   fv = future value
@@ -38,35 +38,35 @@ module Stater
       def y(pv, fv, r, n)
         Math.log(fv / pv) / (n * Math.log(1 + (r / n)))
       end
-    
+
       # Converts the nomical interest rate to APR (annual percentage rate)
       #   r = nominal interest rate
       #   n = compounding periods per year
       def i_to_apr(r, n)
         ((1 + (r / n)) ** n) - 1
       end
-    
+
       # Converts APR (annual percentage rate) to the nomical interest rate
       #   apr = annual percentage rate
       #   n = compounding periods per year
       def apr_to_i(apr, n)
         n * ((1 + apr) ** (1/n.to_f) - 1)
       end
-      
+
       # Converts EAR (effective annual rate) to APR (annual percentage rate)
       #   apr = annual percentage rate
       #   n = compounding periods per year
       def apr_to_ear(apr, n)
         ((1 + apr / n)**n) - 1
       end
-      
+
       # Converts APR (annual percentage rate) to EAR (effective annual rate)
       #   ear = effective annual rate
       #   n = compounding periods per year
       def ear_to_apr(ear, n)
         n * ((1 + ear)**(1 / n.to_f) - 1)
       end
-      
+
       alias_method :present_value, :pv
       alias_method :future_value, :fv
       alias_method :interest, :i
